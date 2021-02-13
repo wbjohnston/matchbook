0498c4134059b1ac7b9b0bec7fb4817f86a680e3
Will Johnston <wbjohnston@gmail.com>
2021-02-12 21:29:32 -0800

    create simple echo server

--- services/gateway/src/main.rs
+++ services/gateway/src/main.rs
@@ -1,9 +1,35 @@
 use tokio::net::TcpListener;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use std::env;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>>{
-    println!("Hello, world!");
+    let port: u16 = env::var("PORT")?.parse()?;
+    println!("parsed port = {}", port);
+    let mut listener = TcpListener::bind(format!("0.0.0.0:{}", port)).await?;
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+        tokio::spawn(async move {
+            println!("accepted connection");
+            let mut buf = [0; 1024];
+
+            loop {
+                let n = match socket.read(&mut buf).await {
+                    Ok(n) if n == 0 => return,
+                    Ok(n) => n,
+                    _ => unimplemented!()
+                };
+
+                // Write the data back
+                if let Err(e) = socket.write_all(&buf[0..n]).await {
+                    eprintln!("failed to write to socket; err = {:?}", e);
+                    return;
+                }
+            }
+
+        });
+    }
 
     Ok(())
 }
