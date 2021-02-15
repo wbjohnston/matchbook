ffda4fb888d70097c42181d18bf9543b923d6bbc
Will Johnston <wbjohnston@gmail.com>
2021-02-14 20:54:56 -0800

    initial gateway

--- services/gateway/src/main.rs
+++ services/gateway/src/main.rs
@@ -1,15 +1,25 @@
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener, UdpSocket};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use std::env;
+use std::{env, net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr, SocketAddrV4}};
+use socket2::{Domain, Protocol, SockAddr, Socket, Type};
+use std::time::Duration;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>>{
     let port: u16 = env::var("PORT")?.parse()?;
-    println!("parsed port = {}", port);
-    let mut listener = TcpListener::bind(format!("0.0.0.0:{}", port)).await?;
+    println!("starting listening on port {}", port);
+    let tcp_listener_addr: SocketAddrV4 = format!("0.0.0.0:{}", port).parse()?;
+    let tcp_listener = TcpListener::bind(tcp_listener_addr).await?;
+    println!("started listening at address = {}", tcp_listener_addr);
+
+    println!("starting udp multicast emmitter at {}", 1234);
+    let udp_tx_addr = Ipv4Addr::new(224, 0, 0, 123);
+    assert!(udp_tx_addr.is_multicast());
+    // let udp_emitter = UdpSocket::bind(udp_tx_addr).await?;
+    println!("started udp multicast emitter at {}", 1234);
 
     loop {
-        let (mut socket, _) = listener.accept().await?;
+        let (mut socket, _) = tcp_listener.accept().await?;
         tokio::spawn(async move {
             println!("accepted connection");
             let mut buf = [0; 1024];
@@ -22,7 +32,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>>{
                 };
 
                 // Write the data back
-                if let Err(e) = socket.write_all(&buf[0..n]).await {
+                if let Err(e) = udp_emitter.send(&buf[0..n]).await {
                     eprintln!("failed to write to socket; err = {:?}", e);
                     return;
                 }
@@ -30,6 +40,4 @@ async fn main() -> Result<(), Box<dyn std::error::Error>>{
 
         });
     }
-
-    Ok(())
 }
