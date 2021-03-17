807a9761251106750c6bce8a4e051b6ff2ddd93d
Will Johnston <wbjohnston@gmail.com>
2021-03-16 19:52:27 -0700

    [retransmitter] test that messages can be resent (#63)
    1. checks that message can be resent out of order

--- services/retransmitter/src/main.rs
+++ services/retransmitter/src/main.rs
@@ -1,6 +1,10 @@
 #![deny(clippy::all)]
+
+use std::marker;
+
+use marker::Unpin;
 use {
-    futures::{SinkExt, StreamExt},
+    futures::{Sink, SinkExt, Stream, StreamExt},
     matchbook_types::*,
     matchbook_util::*,
     std::{
@@ -28,15 +32,23 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         )?;
 
         let socket = UdpSocket::from_std(socket)?;
-
         UdpFramed::new(socket, MatchbookMessageCodec::new())
     };
 
     info!("started listening");
 
-    let (mut sink, mut stream) = udp_socket.split();
-    let mut cache = HashMap::new();
+    let (sink, stream) = udp_socket.split();
 
+    spawn_main_loop(stream, sink, multicast_addr).await;
+    Ok(())
+}
+
+pub async fn spawn_main_loop<St, Si>(mut stream: St, mut sink: Si, multicast_addr: SocketAddr)
+where
+    St: Stream<Item = Result<(Message, SocketAddr), std::io::Error>> + Unpin,
+    Si: Sink<(Message, SocketAddr)> + Unpin,
+{
+    let mut cache = HashMap::new();
     loop {
         let message = stream.next().await;
 
@@ -47,7 +59,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 match message.kind {
                     MessageKind::RetransmitRequest => {
                         if let Some(msg) = cache.get(&message.id).cloned() {
-                            sink.send((msg, multicast_addr)).await?;
+                            let _ = sink.send((msg, multicast_addr)).await;
                         }
                     }
                     // we don't want to cache any retransmit requests, or any message that we've already seen
@@ -60,5 +72,102 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
             _ => continue,
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use futures::SinkExt;
+    use futures_test::*;
+
+    #[tokio::test]
+    async fn sucks_up_events() {
+        let to_retransmit_id_1 = MessageId {
+            publisher_id: ServiceId {
+                kind: ServiceKind::Port,
+                number: 0,
+            },
+            topic_id: "client1".to_owned(),
+            topic_sequence_n: 0,
+        };
+
+        let to_retransmit_1 = Message {
+            id: to_retransmit_id_1.clone(),
+            sending_time: chrono::Utc::now(),
+            kind: MessageKind::LimitOrderSubmitRequest {
+                side: Side::Bid,
+                price: 100,
+                quantity: 100,
+                symbol: ['A', 'D', 'B', 'E'],
+            },
+        };
+
+        let to_retransmit_id_2 = MessageId {
+            publisher_id: ServiceId {
+                kind: ServiceKind::Port,
+                number: 0,
+            },
+            topic_id: "client2".to_owned(),
+            topic_sequence_n: 0,
+        };
+
+        let to_retransmit_2 = Message {
+            id: to_retransmit_id_2.clone(),
+            sending_time: chrono::Utc::now(),
+            kind: MessageKind::LimitOrderSubmitRequest {
+                side: Side::Bid,
+                price: 100,
+                quantity: 100,
+                symbol: ['A', 'D', 'B', 'E'],
+            },
+        };
+
+        let retransmit_1_req = Message {
+            id: to_retransmit_id_1.clone(),
+            kind: MessageKind::RetransmitRequest,
+            sending_time: chrono::Utc::now(),
+        };
+
+        let retransmit_2_req = Message {
+            id: to_retransmit_id_2.clone(),
+            kind: MessageKind::RetransmitRequest,
+            sending_time: chrono::Utc::now(),
+        };
+
+        let (sink_tx, mut sink_rx) = futures::channel::mpsc::channel(1);
+        let (mut stream_tx, stream_rx) = futures::channel::mpsc::channel(1);
+
+        let multicast_addr =
+            SocketAddr::new(DEFAULT_MULTICAST_ADDRESS.into(), DEFAULT_MULTICAST_PORT);
+
+        tokio::spawn(async move { spawn_main_loop(stream_rx, sink_tx, multicast_addr).await });
+
+        // send messages for retrieval
+        stream_tx
+            .send(Ok((to_retransmit_1.clone(), multicast_addr)))
+            .await
+            .unwrap();
+
+        stream_tx
+            .send(Ok((to_retransmit_2.clone(), multicast_addr)))
+            .await
+            .unwrap();
+
+        assert_stream_pending!(sink_rx);
+
+        // request to retransmit
+        stream_tx
+            .send(Ok((retransmit_2_req, multicast_addr)))
+            .await
+            .unwrap();
+
+        stream_tx
+            .send(Ok((retransmit_1_req, multicast_addr)))
+            .await
+            .unwrap();
+
+        assert_stream_next!(sink_rx, (to_retransmit_2, multicast_addr));
+        assert_stream_next!(sink_rx, (to_retransmit_1, multicast_addr));
     }
 }
